b4ab26c7906eacbd21cb252ada3e65c361c7fdc6
Joey Chen <jay.hatelove@gmail.com>
2019-03-16 18:11:36 +0800

    complete MyOrderedEnumerable class

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -2,21 +2,70 @@
 using Lab.Entities;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
-    public class CombineKeyComparer : IComparer<Employee>
+    public class MyOrderedEnumerable : IOrderedEnumerable<Employee>
     {
-        public CombineKeyComparer(Func<Employee, string> keySelector, IComparer<string> keyComparer)
+        private readonly IEnumerable<Employee> _source;
+        private readonly IComparer<Employee> _untilNowComboComparer;
+
+        public MyOrderedEnumerable(IEnumerable<Employee> source, IComparer<Employee> untilNowComboComparer)
+        {
+            _source = source;
+            _untilNowComboComparer = untilNowComboComparer;
+        }
+
+        public IOrderedEnumerable<Employee> CreateOrderedEnumerable<TKey>(Func<Employee, TKey> keySelector,
+            IComparer<TKey> lastComparer, bool @descending)
+        {
+            var comparer = new CombineKeyComparer<TKey>(keySelector, lastComparer);
+            return new MyOrderedEnumerable(_source, new ComboComparer(_untilNowComboComparer, comparer));
+        }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            //bubble sort
+            var elements = _source.ToList();
+            while (elements.Any())
+            {
+                var minElement = elements[0];
+                var index = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    var element = elements[i];
+
+                    if (_untilNowComboComparer.Compare(element, minElement) < 0)
+                    {
+                        minElement = element;
+                        index = i;
+                    }
+                }
+
+                elements.RemoveAt(index);
+                yield return minElement;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class CombineKeyComparer<TKey> : IComparer<Employee>
+    {
+        public CombineKeyComparer(Func<Employee, TKey> keySelector, IComparer<TKey> keyComparer)
         {
             KeySelector = keySelector;
             KeyComparer = keyComparer;
         }
 
-        private Func<Employee, string> KeySelector { get; set; }
-        private IComparer<string> KeyComparer { get; set; }
+        private Func<Employee, TKey> KeySelector { get; set; }
+        private IComparer<TKey> KeyComparer { get; set; }
 
         public int Compare(Employee element, Employee minElement)
         {
@@ -50,30 +99,29 @@ namespace CSharpAdvanceDesignTests
     [TestFixture]
     public class JoeyOrderByTests
     {
-        //[Test]
-        //[Ignore("temp")]
-        //public void orderBy_lastName()
-        //{
-        //    var employees = new[]
-        //    {
-        //        new Employee {FirstName = "Joey", LastName = "Wang"},
-        //        new Employee {FirstName = "Tom", LastName = "Li"},
-        //        new Employee {FirstName = "Joseph", LastName = "Chen"},
-        //        new Employee {FirstName = "Joey", LastName = "Chen"},
-        //    };
+        [Test]
+        public void orderBy_lastName()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
 
-        //    var actual = JoeyOrderByLastName(employees, element => element.LastName);
+            var actual = JoeyOrderByKeepComparer(employees, element => element.LastName, Comparer<string>.Default);
 
-        //    var expected = new[]
-        //    {
-        //        new Employee {FirstName = "Joseph", LastName = "Chen"},
-        //        new Employee {FirstName = "Joey", LastName = "Chen"},
-        //        new Employee {FirstName = "Tom", LastName = "Li"},
-        //        new Employee {FirstName = "Joey", LastName = "Wang"},
-        //    };
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Wang"},
+            };
 
-        //    expected.ToExpectedObject().ShouldMatch(actual);
-        //}
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
 
         [Test]
         public void orderBy_lastName_and_firstName()
@@ -86,11 +134,13 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
 
-            var firstComparer = new CombineKeyComparer(element => element.LastName, Comparer<string>.Default);
-            var secondComparer = new CombineKeyComparer(element => element.FirstName, Comparer<string>.Default);
+            var firstComparer = new CombineKeyComparer<string>(element => element.LastName, Comparer<string>.Default);
+            var secondComparer = new CombineKeyComparer<string>(element => element.FirstName, Comparer<string>.Default);
 
-            var actual = JoeyOrderByLastNameAndFirstName(employees,
-                new ComboComparer(firstComparer, secondComparer));
+            var firstCombo = new ComboComparer(firstComparer, secondComparer);
+
+            var actual = JoeyOrderBy(employees,
+                firstCombo);
 
             var expected = new[]
             {
@@ -103,9 +153,63 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(
+        [Test]
+        public void lastName_firstName_Age()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 50},
+                new Employee {FirstName = "Tom", LastName = "Li", Age = 31},
+                new Employee {FirstName = "Joseph", LastName = "Chen", Age = 32},
+                new Employee {FirstName = "Joey", LastName = "Chen", Age = 33},
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 20},
+            };
+
+            var orderByLastName = JoeyOrderByKeepComparer(employees, e => e.LastName, Comparer<string>.Default);
+
+            var orderByFirstName = JoeyThenBy(orderByLastName, e => e.FirstName, Comparer<string>.Default);
+
+            var actual = JoeyThenBy(orderByFirstName, e => e.Age, Comparer<int>.Default);
+            //var firstComparer = new CombineKeyComparer<string>(element => element.LastName, Comparer<string>.Default);
+            //var secondComparer = new CombineKeyComparer<string>(element => element.FirstName, Comparer<string>.Default);
+
+            //var firstCombo = new ComboComparer(firstComparer, secondComparer);
+
+            //var thirdComparer = new CombineKeyComparer<int>(element => element.Age, Comparer<int>.Default);
+
+            //var finalCombo = new ComboComparer(firstCombo, thirdComparer);
+
+            //var actual = JoeyOrderBy(employees, finalCombo);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen", Age = 33},
+                new Employee {FirstName = "Joseph", LastName = "Chen", Age = 32},
+                new Employee {FirstName = "Tom", LastName = "Li", Age = 31},
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 20},
+                new Employee {FirstName = "Joey", LastName = "Wang", Age = 50},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IOrderedEnumerable<Employee> JoeyThenBy<TKey>(IOrderedEnumerable<Employee> source,
+            Func<Employee, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            return source.CreateOrderedEnumerable(keySelector, comparer, false);
+        }
+
+        private IOrderedEnumerable<Employee> JoeyOrderByKeepComparer(IEnumerable<Employee> employees,
+            Func<Employee, string> keySelector, Comparer<string> comparer)
+        {
+            var myOrderedEnumerable =
+                new MyOrderedEnumerable(employees, new CombineKeyComparer<string>(keySelector, comparer));
+            return myOrderedEnumerable;
+        }
+
+        private IEnumerable<Employee> JoeyOrderBy(
             IEnumerable<Employee> employees,
-            ComboComparer comboComparer)
+            IComparer<Employee> comparer)
         {
             //bubble sort
             var elements = employees.ToList();
@@ -117,27 +221,11 @@ namespace CSharpAdvanceDesignTests
                 {
                     var element = elements[i];
 
-                    if (comboComparer.Compare(element, minElement) < 0)
+                    if (comparer.Compare(element, minElement) < 0)
                     {
                         minElement = element;
                         index = i;
                     }
-
-                    //var firstCompareResult = comboComparer.FirstComparer.Compare(element, minElement);
-                    //if (firstCompareResult < 0)
-                    //{
-                    //    minElement = element;
-                    //    index = i;
-                    //}
-                    //else if (firstCompareResult == 0)
-                    //{
-                    //    var secondCompareResult = comboComparer.SecondComparer.Compare(element, minElement);
-                    //    if (secondCompareResult < 0)
-                    //    {
-                    //        minElement = element;
-                    //        index = i;
-                    //    }
-                    //}
                 }
 
                 elements.RemoveAt(index);
--- Lab/Entities/Employee.cs
+++ Lab/Entities/Employee.cs
@@ -5,5 +5,6 @@
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public Role Role { get; set; }
+        public int Age { get; set; }
     }
 }
