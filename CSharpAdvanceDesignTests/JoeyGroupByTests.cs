5701f6376898d438d11331c1ec0c6ed8e16acea1
Joey Chen <jay.hatelove@gmail.com>
2019-03-17 16:49:36 +0800

    Group: prototype

--- CSharpAdvanceDesignTests/JoeyGroupByTests.cs
+++ CSharpAdvanceDesignTests/JoeyGroupByTests.cs
@@ -1,14 +1,13 @@
 ﻿using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyGroupByTests
     {
         [Test]
@@ -37,7 +36,57 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<IGrouping<string, Employee>> JoeyGroupBy(IEnumerable<Employee> employees)
         {
-            throw new NotImplementedException();
+            var lookup = new Dictionary<string, List<Employee>>();
+
+            var employeEnumerator = employees.GetEnumerator();
+            while (employeEnumerator.MoveNext())
+            {
+                var employee = employeEnumerator.Current;
+
+                if (lookup.ContainsKey(employee.LastName))
+                {
+                    lookup[employee.LastName].Add(employee);
+                }
+                else
+                {
+                    lookup.Add(employee.LastName, new List<Employee> { employee });
+                }
+            }
+
+            return ConvertMultiGrouping(lookup);
         }
+
+        private IEnumerable<IGrouping<string, Employee>> ConvertMultiGrouping(Dictionary<string, List<Employee>> lookup)
+        {
+            var enumerator = lookup.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var keyValuePair = enumerator.Current;
+                yield return new MyGrouping(keyValuePair.Key, keyValuePair.Value);
+            }
+        }
+    }
+
+    public class MyGrouping : IGrouping<string, Employee>
+    {
+        private readonly IEnumerable<Employee> _collection;
+
+        public MyGrouping(string key, IEnumerable<Employee> collection)
+        {
+            Key = key;
+            _collection = collection;
+        }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            return _collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public string Key { get; }
     }
 }
